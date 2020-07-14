daad9df42e6bf7cd1c4f9f6a26b37f550230f510
chiheiwei1980 <64567857+chiheiwei1980@users.noreply.github.com>
2020-07-14 23:20:36 +0800

    0714

---
+++ jj.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
